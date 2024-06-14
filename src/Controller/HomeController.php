c5e96d7e5afdcce3ba0a17657b83396055846cb6
Cédric Phung <cedric3011@outlook.fr>
2024-06-14 12:55:36 +0200

    create side panel for reviews

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ReviewRepository $reviews ): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'lastreview' => $reviews->findOneBy([], ['id' => 'desc']),
+            'reviews' => $reviews->findAll()
         ]);
     }
 }
