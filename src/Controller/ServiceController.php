fe509af055da66165f67e5b01908a7a79eeeb538
Cédric Phung <cedric3011@outlook.fr>
2024-06-15 11:09:04 +0200

    add sevice retrieval

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class ServiceController extends AbstractController
 {
     #[Route('/service', name: 'app_service')]
-    public function index(): Response
+    public function index(ServiceRepository $services): Response
     {
         return $this->render('service/index.html.twig', [
             'controller_name' => 'ServiceController',
+            'services' => $services->findAll(),
         ]);
     }
 }
