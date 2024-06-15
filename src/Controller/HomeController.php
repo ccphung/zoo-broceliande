e34d424204d5330e6a8163d5ca41b7a09d3d78e3
Cédric Phung <cedric3011@outlook.fr>
2024-06-15 14:58:35 +0200

    fetch data from database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ReviewRepository;
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ReviewRepository $reviews ): Response
+    public function index(ReviewRepository $reviews, ServiceRepository $services): Response
     {
         return $this->render('home/index.html.twig', [
             'lastreview' => $reviews->findOneBy([], ['id' => 'desc']),
-            'reviews' => $reviews->findAll()
+            'reviews' => $reviews->findAll(),
+            'services' => $services->findAll()
         ]);
     }
 }
