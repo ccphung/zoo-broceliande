91de54b864567694139af8754b658bd451343a04
Cédric Phung <cedric3011@outlook.fr>
2024-06-15 10:34:37 +0200

    configure vich uploader

--- src/Controller/Admin/ServiceCrudController.php
+++ src/Controller/Admin/ServiceCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Service;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +16,15 @@ class ServiceCrudController extends AbstractCrudController
         return Service::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('name'),
             TextEditorField::new('description'),
+            TextField::new('imageName')
+                ->setLabel('Nom de l\'image'),
+            TextField::new('imageFile')
+                ->setFormType(VichFileType::class),
         ];
     }
-    */
 }
