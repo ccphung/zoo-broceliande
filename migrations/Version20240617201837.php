ca16e0eb851b90a16fcc2815fe3fd14cbacce9a9
Cédric Phung <cedric3011@outlook.fr>
2024-06-17 22:19:05 +0200

    add new fields to habitat entity

---
+++ migrations/Version20240617201837.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240617201837 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE habitat ADD image_name VARCHAR(255) NOT NULL, ADD updated_at DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE habitat DROP image_name, DROP updated_at');
+    }
+}
