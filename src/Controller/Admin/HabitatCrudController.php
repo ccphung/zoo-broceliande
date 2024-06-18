e84cdacdf21e423042abbb0db951f38b75161341
Cédric Phung <cedric3011@outlook.fr>
2024-06-18 10:33:43 +0200

    create fields

--- src/Controller/Admin/HabitatCrudController.php
+++ src/Controller/Admin/HabitatCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Habitat;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class HabitatCrudController extends AbstractCrudController
 {
@@ -15,14 +18,27 @@ class HabitatCrudController extends AbstractCrudController
         return Habitat::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud) : Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Habitats')
+            ->setEntityLabelInSingular('Habitat')
+
+            ->setPageTitle("index", "Gestion des habitats")
+            ->setPageTitle("edit", "Gestion d'un habitat");
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name')
+                ->setLabel('Nom'),
+            TextareaField::new('description')
+                ->setLabel('Description'),
+            TextField::new('imageName')
+                ->setLabel('Nom de l\'image'),
+            TextField::new('imageFile')
+                ->setFormType(VichFileType::class),
         ];
     }
-    */
 }
