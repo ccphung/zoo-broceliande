c09debd1e6e408ba184c758b07e73c05fb2fc508
Cédric Phung <cedric3011@outlook.fr>
2024-06-18 20:50:40 +0200

    add fetch method

--- src/Controller/HabitatController.php
+++ src/Controller/HabitatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\HabitatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HabitatController extends AbstractController
 {
     #[Route('/habitat', name: 'app_habitat')]
-    public function index(): Response
+    public function index(HabitatRepository $habitats): Response
     {
         return $this->render('habitat/index.html.twig', [
             'controller_name' => 'HabitatController',
+            'habitats' => $habitats->findAll()
         ]);
     }
 }
