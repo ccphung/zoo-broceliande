fda9e4b2e34e801fc5781d49b4aa16c9a9e622e5
Cédric Phung <cedric3011@outlook.fr>
2024-06-18 10:34:47 +0200

    fetch habitat data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\HabitatRepository;
 use App\Repository\ReviewRepository;
 use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,12 +12,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ReviewRepository $reviews, ServiceRepository $services): Response
+    public function index(ReviewRepository $reviews, ServiceRepository $services, HabitatRepository $habitats): Response
     {
+        $lastFourHabitats = $habitats->findLastFour();
+
         return $this->render('home/index.html.twig', [
             'lastreview' => $reviews->findOneBy([], ['id' => 'desc']),
             'reviews' => $reviews->findAll(),
-            'services' => $services->findAll()
+            'services' => $services->findAll(),
+            'habitats' => $lastFourHabitats,
         ]);
     }
 }
