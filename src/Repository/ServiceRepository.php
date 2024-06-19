4182d5d4265e02313d77b525bbb24be3d200b007
Cédric Phung <cedric3011@outlook.fr>
2024-06-19 14:05:42 +0200

    create filter on services

--- src/Repository/ServiceRepository.php
+++ src/Repository/ServiceRepository.php
@@ -16,28 +16,17 @@ class ServiceRepository extends ServiceEntityRepository
         parent::__construct($registry, Service::class);
     }
 
-//    /**
-//     * @return Service[] Returns an array of Service objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findByFilter($categoryFilter) 
 
-//    public function findOneBySomeField($value): ?Service
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    {
+        $query = $this->createQueryBuilder('s');
+
+        if ($categoryFilter !== null) {
+            $query
+                ->andWhere('s.category = :category')
+                ->setParameter('category', "$categoryFilter");
+        }
+
+        return $query->getQuery()->getResult();
+    }
 }
