2339c51e261087d0ce5dbc6d0deedbceca74e256
Cédric Phung <cedric3011@outlook.fr>
2024-06-19 14:05:42 +0200

    create filter on services

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -1,20 +1,42 @@
-<?php
+<?php 
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ServiceController extends AbstractController
 {
     #[Route('/service', name: 'app_service')]
-    public function index(ServiceRepository $services): Response
+    public function index(ServiceRepository $services, CategoryRepository $categories, Request $request): Response
     {
-        return $this->render('service/index.html.twig', [
-            'controller_name' => 'ServiceController',
-            'services' => $services->findAll(),
-        ]);
+        $categoryFilter = $request->get('category');
+        
+        if ($request->isXmlHttpRequest()) {
+            // Requête Ajax
+            if ($categoryFilter === "all") {
+                $servicesFiltered = $services->findAll();
+            } else {
+                $servicesFiltered = $services->findByFilter($categoryFilter);
+            }
+            
+            return new JsonResponse([
+                'content' => $this->renderView('service/_content.html.twig', [
+                    'services' => $servicesFiltered,
+                    'categories' => $categories->findAll()
+                ])
+            ]);
+        } else {
+            // Requête normale 
+            return $this->render('service/index.html.twig', [
+                'services' => $services->findAll(),
+                'categories' => $categories->findAll()
+            ]);
+        }
     }
 }
