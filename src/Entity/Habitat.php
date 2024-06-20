b31e3a63c75a3110214a5bed8a35d97c3373e5fe
Cédric Phung <cedric3011@outlook.fr>
2024-06-20 17:16:52 +0200

    add toString method

--- src/Entity/Habitat.php
+++ src/Entity/Habitat.php
@@ -152,4 +152,9 @@ class Habitat
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
--- src/Entity/Species.php
+++ src/Entity/Species.php
@@ -75,4 +75,9 @@ class Species
 
         return $this;
     }
+    
+    public function __toString()
+    {
+        return $this->label;
+    }
 }
