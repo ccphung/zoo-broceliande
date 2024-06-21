b1208886856610f8e1c64090bdae9909e6b35ab3
Cédric Phung <cedric3011@outlook.fr>
2024-06-21 10:05:47 +0200

    add reference

--- src/DataFixtures/HabitatFixtures.php
+++ src/DataFixtures/HabitatFixtures.php
@@ -12,6 +12,7 @@ class HabitatFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $jungle = new Habitat();
+        $this->addReference('Jungle', $jungle );
         $jungle->setName('Jungle Enchantée');
         $jungle->setDescription("Entrez dans la Jungle Enchantée, une partie captivante de notre zoo où la nature luxuriante et les sons exotiques vous transportent au cœur des forêts tropicales. Découvrez une végétation dense, des arbres majestueux et des plantes exotiques qui recréent fidèlement l'habitat naturel de nombreux animaux fascinants.");
         $jungle->setImageName('jungle.jpg');
@@ -19,6 +20,7 @@ class HabitatFixtures extends Fixture
         $manager->persist($jungle);
 
         $forest = new Habitat();
+        $this->addReference('Forêt', $forest );
         $forest->setName('Forêt Mystérieuse');
         $forest->setDescription("Plongez au cœur de la Forêt Mystérieuse, un coin enchanteur de notre zoo où la nature sauvage et la tranquillité se rencontrent. Promenez-vous parmi les arbres imposants, les ruisseaux sinueux et la végétation verdoyante qui recréent fidèlement l'habitat naturel de nombreux animaux fascinants.");
         $forest->setImageName('forest.jpg');
@@ -26,6 +28,7 @@ class HabitatFixtures extends Fixture
         $manager->persist($forest);
 
         $ocean = new Habitat();
+        $this->addReference('Océan', $ocean );
         $ocean->setName('Royaume de l\'Océan');
         $ocean->setDescription("Plongez dans le Royaume de l'Océan, une section fascinante de notre zoo où les mystères des profondeurs marines se dévoilent devant vos yeux. Explorez les vastes environnements aquatiques recréés pour accueillir une incroyable diversité de vie marine.");
         $ocean->setImageName('ocean.jpg');
@@ -33,6 +36,7 @@ class HabitatFixtures extends Fixture
         $manager->persist($ocean);
 
         $savannah = new Habitat();
+        $this->addReference('Savane', $savannah );
         $savannah->setName('Savane Sauvage');
         $savannah->setDescription("La Savane Sauvage n'est pas seulement un lieu d'émerveillement, mais aussi d'apprentissage. Informez-vous sur les défis auxquels la faune de la savane est confrontée, comme le braconnage et la perte d'habitat, et découvrez les initiatives de conservation mises en place pour protéger ces animaux emblématiques.");
         $savannah->setImageName('savannah.jpg');
