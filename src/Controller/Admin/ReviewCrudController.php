8bb92db36f32b40e01261697bbd44264659460f3
Cédric Phung <cedric3011@outlook.fr>
2024-06-21 15:22:28 +0200

    configure review crud

--- src/Controller/Admin/ReviewCrudController.php
+++ src/Controller/Admin/ReviewCrudController.php
@@ -3,10 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Review;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class ReviewCrudController extends AbstractCrudController
 {
@@ -15,14 +20,30 @@ class ReviewCrudController extends AbstractCrudController
         return Review::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud) : Crud 
+    {
+        return $crud
+            ->setEntityLabelInPlural('Avis')
+            ->setEntityLabelInSingular('Avis')
+
+            ->setPageTitle("index", "Gestion des avis")
+            ->setPageTitle("edit", "Gestion d'un avis")
+
+            ->setPaginatorPageSize(10);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('pseudo')
+                ->setLabel('Pseudo'),
+            TextareaField::new('comment')
+                ->setLabel('Commentaire'),
+            DateField::new('date')
+                ->setLabel('Date'),
+            BooleanField::new('isVisible')
+                ->setLabel('Approuvé'),
         ];
     }
-    */
+
 }
