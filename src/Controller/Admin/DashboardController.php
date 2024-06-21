155aa3cbbb577d893f0a266ed5f30b8720cd27fa
Cédric Phung <cedric3011@outlook.fr>
2024-06-21 15:16:32 +0200

    add access restrictions

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,22 +18,6 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        // return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
         return $this->render('admin/dashboard.html.twig');
     }
 
@@ -47,10 +31,15 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Employés/Vétérinaires', 'fa-solid fa-person-digging', User::class);
-        yield MenuItem::linkToCrud('Animal', 'fa-solid fa-otter', Animal::class);
-        yield MenuItem::linkToCrud('Services', 'fa-solid fa-train', Service::class);
-        yield MenuItem::linkToCrud('Habitat', 'fa-solid fa-solid fa-house', Habitat::class);
-        yield MenuItem::linkToCrud('Horaires', 'fa-regular fa-clock', OpeningHours::class);
+        yield MenuItem::linkToCrud('Employés/Vétérinaires', 'fa-solid fa-person-digging', User::class)
+        ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Animal', 'fa-solid fa-otter', Animal::class)
+        ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Services', 'fa-solid fa-train', Service::class)
+        ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Habitat', 'fa-solid fa-solid fa-house', Habitat::class)
+        ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Horaires', 'fa-regular fa-clock', OpeningHours::class)
+        ->setPermission('ROLE_ADMIN');
     }
 }
