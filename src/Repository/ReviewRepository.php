74fe27104c785f617c777a021071694268fd32e7
Cédric Phung <cedric3011@outlook.fr>
2024-06-21 15:55:08 +0200

    add find unapproved comment method

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -16,28 +16,12 @@ class ReviewRepository extends ServiceEntityRepository
         parent::__construct($registry, Review::class);
     }
 
-//    /**
-//     * @return Review[] Returns an array of Review objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Review
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findUnapprovedReviewsCount(): int
+    {
+        return $this->createQueryBuilder('r')
+            ->select('COUNT(r.id)')
+            ->where('r.isVisible = false')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 }
