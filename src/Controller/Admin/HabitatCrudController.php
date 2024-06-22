a2a4740c1caba3500a76bc4283d4d7b5b6842ce9
Cédric Phung <cedric3011@outlook.fr>
2024-06-22 16:27:41 +0200

    disable fields according to the role

--- src/Controller/Admin/HabitatCrudController.php
+++ src/Controller/Admin/HabitatCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Habitat;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Vich\UploaderBundle\Form\Type\VichFileType;
@@ -19,6 +22,13 @@ class HabitatCrudController extends AbstractCrudController
     {
         return Habitat::class;
     }
+    
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
 
     public function configureCrud(Crud $crud) : Crud
     {
@@ -30,17 +40,43 @@ class HabitatCrudController extends AbstractCrudController
             ->setPageTitle("edit", "Gestion d'un habitat");
     }
 
+    public function configureActions(Actions $actions): Actions
+{
+
+    return $actions
+        
+        ->setPermission(Action::NEW, 'ROLE_ADMIN')
+    ;
+}
+
     public function configureFields(string $pageName): iterable
     {
+        $isVet = $this->security->isGranted('ROLE_VET');
+        $isAdmin = $this->security->isGranted('ROLE_ADMIN');
+
         return [
             TextField::new('name')
-                ->setLabel('Nom'),
+                ->setLabel('Nom')
+                ->setDisabled($isVet),
+
             TextareaField::new('description')
-                ->setLabel('Description'),
+                ->setLabel('Description')
+                ->setDisabled($isVet),
+
             TextField::new('imageName')
-                ->setLabel('Nom de l\'image'),
+                ->setLabel('Nom de l\'image')
+                ->hideOnIndex()
+                ->setDisabled($isVet),
+
             TextField::new('imageFile')
-                ->setFormType(VichFileType::class),
+                ->setLabel('Image')
+                ->setFormType(VichFileType::class)
+                ->hideOnIndex()
+                ->setDisabled($isVet),
+
+            TextareaField::new('habitatRemark')
+                ->setLabel('Remarque sur l\'habitat')
+                ->setDisabled($isAdmin),
         ];
     }
 }
