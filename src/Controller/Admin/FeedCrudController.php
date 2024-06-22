a6791c24d5266b56cf6c047c9b06f091470820ba
Cédric Phung <cedric3011@outlook.fr>
2024-06-22 21:04:29 +0200

    add filters

--- src/Controller/Admin/FeedCrudController.php
+++ src/Controller/Admin/FeedCrudController.php
@@ -3,24 +3,47 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Feed;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Positive;
 
-#[IsGranted('ROLE_EMPLOYE')]
+#[IsGranted(new Expression('is_granted("ROLE_EMPLOYE") or is_granted("ROLE_VET")'))]
 
 class FeedCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Feed::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('animal')
+            ->add('date');
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+    
+        return $actions
+            
+            ->setPermission(Action::NEW, 'ROLE_EMPLOYE')
+            ->setPermission(Action::EDIT, 'ROLE_EMPLOYE')
+            ->setPermission(Action::DELETE, 'ROLE_EMPLOYE')
+        ;
     }
 
     public function configureFields(string $pageName): iterable
@@ -32,8 +55,17 @@ class FeedCrudController extends AbstractCrudController
                 ->setFormTypeOption('constraints', [
                     new NotBlank(['message' => 'Ce champ ne peut pas être vide'])
                 ]),
+
+            TextField::new('animal.species', 'Species')
+                ->setLabel('Espèce')
+                ->onlyOnIndex()
+                ->setRequired(false),
+
             AssociationField::new('food')
-                ->setLabel('Nourriture'),
+                ->setLabel('Nourriture')
+                ->setFormTypeOption('constraints', [
+                    new NotBlank(['message' => 'Ce champ ne peut pas être vide'])
+                ]),
             IntegerField::new('quantity')
                 ->setLabel('Quantité (en grammes)')
                 ->setFormTypeOption('constraints', [
