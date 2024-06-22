280a2632d7414eb0d7e60ac1914dfd0647c62373
Cédric Phung <cedric3011@outlook.fr>
2024-06-22 21:05:26 +0200

    modify permissions

--- src/Controller/Admin/HabitatCrudController.php
+++ src/Controller/Admin/HabitatCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Habitat;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -42,13 +43,14 @@ class HabitatCrudController extends AbstractCrudController
     }
 
     public function configureActions(Actions $actions): Actions
-{
+    {
 
     return $actions
         
         ->setPermission(Action::NEW, 'ROLE_ADMIN')
+        ->setPermission(Action::DELETE, 'ROLE_ADMIN')
     ;
-}
+    }
 
     public function configureFields(string $pageName): iterable
     {
