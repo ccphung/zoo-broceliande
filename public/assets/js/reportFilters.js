52316d866edcacba53460a6e93fa409f5f7a7561
Cédric Phung <cedric3011@outlook.fr>
2024-06-24 16:23:40 +0200

    create filters

---
+++ public/assets/js/reportFilters.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+
+    const filtersForm = document.getElementById('filters');
+
+    document.querySelectorAll("#filters input, #filters select").forEach(element => {
+        element.addEventListener('change', () => {
+            const form = new FormData(filtersForm);
+            const params = new URLSearchParams();
+
+            form.forEach((value, key) => {
+                params.append(key, value);
+            });
+            params.append('ajax', '1');
+
+            const url = new URL(window.location.href);
+            
+            url.search = params.toString();
+
+            fetch(url.pathname  + url.search, {
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest"
+                }
+            })
+            .then(response => response.json())
+
+            .then(data => {
+                const content = document.getElementById("content");
+                content.innerHTML = data.content;
+                history.pushState({}, null, url.pathname + "?" + url.search);
+            })
+            .catch(e => console.log(e));
+        });
+    });
+};
