9bb6e5c80366182524ceccc4165e367f914e4762
Cédric Phung <cedric3011@outlook.fr>
2024-06-24 16:23:40 +0200

    create filters

--- src/Controller/VetReportController.php
+++ src/Controller/VetReportController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\AnimalRepository;
 use App\Repository\VetReportRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class VetReportController extends AbstractController
 {
     #[Route('/vetreport', name: 'app_vet_report')]
-    public function index(VetReportRepository $vetreports): Response
+    public function index(VetReportRepository $vetreports, AnimalRepository $animals, Request $request): Response
     {
-        return $this->render('vet_report/index.html.twig', [
-            'reports' => $vetreports->findall()
-        ]);
+        $filterDate = $request->get('date');
+        $filterAnimal = $request->get('animal');
+
+        if($request->isXmlHttpRequest()){
+            
+            return new JsonResponse([
+                'content' => $this->renderView('vet_report/_content.html.twig', [
+                    'reports' => $vetreports->findByFilter($filterAnimal, $filterDate)
+                ])
+            ]);
+        } else {
+            return $this->render('vet_report/index.html.twig', [
+                'reports' => $vetreports->findall(),
+                'animals' => $animals->findall()
+            ]);
+        }
     }
 }
--- src/Repository/VetReportRepository.php
+++ src/Repository/VetReportRepository.php
@@ -16,28 +16,24 @@ class VetReportRepository extends ServiceEntityRepository
         parent::__construct($registry, VetReport::class);
     }
 
-//    /**
-//     * @return VetReport[] Returns an array of VetReport objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('v.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findByFilter($filterAnimal, $filterDate)
+    {
+        $query = $this->createQueryBuilder('v');
 
-//    public function findOneBySomeField($value): ?VetReport
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        if(!empty($filterAnimal)) {
+            $query
+                ->andWhere('v.animal IN (:animals)')
+                ->setParameter(':animals', array_values($filterAnimal));
+        }
+
+        if ($filterDate === "asc") {
+            $query
+                ->orderBy('v.visitDate', "ASC");
+        } elseif ($filterDate === "desc") {
+            $query
+                ->orderBy('v.visitDate', "DESC");
+        }
+
+        return $query->getQuery()->getResult();
+    }
 }
