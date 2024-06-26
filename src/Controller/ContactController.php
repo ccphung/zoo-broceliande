632ae4d780d949cd0e77e18d6e808b0912d8de42
Cédric Phung <cedric3011@outlook.fr>
2024-06-26 16:43:21 +0200

    create contact page

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $email = (new TemplatedEmail())
+                ->from($formData['email'])
+                ->to('votre@email.com')
+                ->subject('Nouveau message depuis le formulaire de contact')
+                ->html('Nom: ' . $formData['lastName'] . '<br>' .
+                       'Prénom: ' . $formData['firstName'] . '<br>' .
+                       'Email: ' . $formData['email'] . '<br>' .
+                       'Message: ' . nl2br($formData['message']));
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé avec succès.');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
