2172a7fb13672e59a61c8d68304914d9b2249a47
Cédric Phung <cedric3011@outlook.fr>
2024-07-12 23:04:29 +0200

    modify file

--- config/mongodb.php
+++ config/mongodb.php
@@ -1,17 +1,13 @@
 <?php
 
-// Récupère l'URL MongoDB depuis les variables d'environnement
 $mongodbUrl = getenv('MONGODB_URL');
 
-// Vérifie si l'URL MongoDB est bien définie
 if (!$mongodbUrl) {
     die("L'URL MongoDB n'est pas définie dans les variables d'environnement.");
 }
 
-// Configuration du proxy
-$proxyUrl = 'tcp://nuzyrlvq72agv5:ieom6200n2pqh2jv2jn1m18x1b6c@eu-west-static-02.quotaguard.com:9293';
+$proxyUrl = getenv('QUOTAGUARDSTATIC_URL');
 
-// Création du manager MongoDB avec les options du proxy
 $manager = new MongoDB\Driver\Manager(
     $mongodbUrl,
     [],
@@ -27,7 +23,6 @@ $manager = new MongoDB\Driver\Manager(
     ]
 );
 
-// Exemple de requête pour tester la connexion
 try {
     $query = new MongoDB\Driver\Query([]);
     $cursor = $manager->executeQuery('database_name.collection_name', $query);
