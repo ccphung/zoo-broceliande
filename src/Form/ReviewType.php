620464c869697d15561a62be45a2d98fa95f5e0f
Cédric Phung <cedric3011@outlook.fr>
2024-07-14 12:34:17 +0200

    modify style

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -23,7 +23,7 @@ class ReviewType extends AbstractType
             ],
             'label' => 'Pseudonyme',
             'label_attr' => [
-                'class' => 'form-label mt-2 fw-bold small-font'
+                'class' => 'form-label mt-2 small-font'
             ],
             'constraints' => [
                 new NotBlank(['message' => 'Veuillez indiquer un pseudo.']),
@@ -39,7 +39,7 @@ class ReviewType extends AbstractType
                 ],
                 'label' => 'Commentaire',
                 'label_attr' => [
-                    'class' => 'form-label mt-2 fw-bold small-font'
+                    'class' => 'form-label mt-2 small-font'
                 ],
                 'constraints' => [
                     new NotBlank(['message' => 'Veuillez ajouter un commentaire']),
