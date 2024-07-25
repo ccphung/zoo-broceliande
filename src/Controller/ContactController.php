5c4d88726ed692e92e78ef3c53197ea8f4ed46f5
Cédric Phung <cedric3011@outlook.fr>
2024-07-25 12:40:37 +0200

    modify email sending to users  with role_employe

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,26 +14,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, MailerInterface $mailer): Response
+    public function index(Request $request, MailerInterface $mailer, UserRepository $user): Response
     {
+        $employees = $user->findEmployees();
+
         $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
+        // var_dump($employees);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $formData = $form->getData();
 
-            $email = (new TemplatedEmail())
+            foreach ($employees as $employee)
+            {
+                $email = (new TemplatedEmail())
                 ->from($formData['email'])
-                ->to('jose@arcadia.fr')
+                ->to($employee->getUsername())
                 ->subject('Nouveau message depuis le formulaire de contact')
                 ->html('Nom: ' . htmlspecialchars($formData['lastName']) . '<br>' .
                        'Prénom: ' . htmlspecialchars($formData['firstName']) . '<br>' .
-                       'Email: ' . htmlspecialchars($formData['email']) . '<br>' .
+                       'Email: ' . htmlspecialchars($formData['email']) . '<br>'.
                        'Sujet: ' . htmlspecialchars($formData['subject']) . '<br>' .
                        'Message: ' . nl2br(htmlspecialchars($formData['message'])));
 
-            $mailer->send($email);
+                $mailer->send($email);
+             }
+
 
             $this->addFlash('success', 'Votre message a été envoyé avec succès.');
 
